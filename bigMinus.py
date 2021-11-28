b0ff0a6f0168642b295590194933498b84462f14
Cleankz <92687535+Cleankz@users.noreply.github.com>
2021-11-28 15:48:54 +0300

    Update bigMinus.py

--- bigMinus.py
+++ bigMinus.py
@@ -1,9 +1,11 @@
-def BigMinus(s1, s2):
-    num_1 = list(s1)
-    num_2 = list(s2)
-    result_list = []
-    
-    
+def column(str_1,str_2,F):
+    if F:
+        num_1 = str_1
+        num_2 = str_2
+    else:
+        num_1 = str_2
+        num_2 = str_1
+        
     for i in range(len(num_1)-1,len(num_1)-len(num_2)-1,-1):
         for j in range(len(num_2)-1,-1,-1):
             if  int(num_1[i]) - int(num_2[j]) == 0:
@@ -19,22 +21,34 @@ def BigMinus(s1, s2):
                 num = (int(num_1[i]) + 10) - int(num_2[j])
                 num_1[i-1] =int(num_1[i-1]) - 1
                 num_1[i] = str(num)
+                del num_2[j]
+                break
             elif int(num_1[i]) < int(num_2[j]) and int(num_1[i-1]) == 0:
                 for n in range(i-1,-1,-1):
                     if int(num_1[n]) !=0:
                         idx = n
                         break
                 num = (int(num_1[i]) + 10) - int(num_2[j])
-                num_1[idx] =int(num_1[idx]) - 1
+                num_1[idx] =str(int(num_1[idx]) - 1)
                 num_1[i] = str(num)
+                del num_2[j]
                 for k in range(i,idx,-1):
                     if int(num_1[k]) == 0:
                         num_1[k] = '9'
-                
-
-
-            
-                
-            
+                break
     return num_1
-print(BigMinus('12345678910110000006','123456'))
+    
+
+
+
+
+def BigMinus(s1, s2):
+    n_1 = list(s1)
+    n_2 = list(s2)
+    result_list = []
+    if len(n_1) > len(n_2):
+        result = ''.join(column(n_1,n_2,True))
+        return result
+    else:
+        result = ''.join(column(n_1,n_2,False))
+        return result
